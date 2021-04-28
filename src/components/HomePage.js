7001e7533ededcb44e278f0247208881e126ee4a
wedhimd <wedhimd@gmail.com>
2021-04-28 12:14:00 +0200

    created about, course and home pages added app header components

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Plularsight Administration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps</p>
+
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
