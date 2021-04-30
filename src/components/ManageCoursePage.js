54f84dc7fd177a6aa3c179790b0813f8a8aad954
wedhimd <wedhimd@gmail.com>
2021-05-01 00:20:51 +0200

    added course form and Text input components

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,41 @@
-import { React } from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  function handleChange({ target }) {
+    const updatedCourse = {
+      ...course,
+      [target.name]: target.value,
+    };
+    setCourse(updatedCourse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved successfuly");
+    });
+  }
+
   return (
     <>
       <h1>Manage Course</h1>
-      {props.match.params.slug}
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
